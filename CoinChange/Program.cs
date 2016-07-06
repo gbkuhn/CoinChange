18964b9ec0cfe7b82eeed9bbe153f65124659fb5
geoffrey.kuhn <kuhn.geoff@gmail.com>
2016-07-06 14:42:55 -0400

    initial commit

---
+++ CoinChange/Program.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using CoinLibrary;
+
+
+namespace CoinChange
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int change;
+
+            Class1 change_prompt = new Class1();
+            change = change_prompt.prompt();
+
+            change_prompt.amount_quarters(change);
+        }
+    }
+}
---
+++ CoinLibrary/Class1.cs
@@ -0,0 +1,48 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CoinLibrary
+{
+    public class Class1
+    {
+        public int amount_q;
+        public int amount_d;
+        public int amount_n;
+        public int amount_p;
+
+        public int prompt()
+        {
+            Console.WriteLine("What is the amount of change you wish to receive from 1 to 99 cents");
+            string amount = Console.ReadLine();
+            return Convert.ToInt32(amount);
+        }
+
+        public int amount_quarters(int change)
+        {
+
+            return amount_q;
+        }
+
+        public int amount_dimes(int change)
+        {
+
+            return amount_d;
+        }
+
+        public int amount_nickels(int change)
+        {
+
+            return amount_n;
+        }
+
+        public int amount_pennies(int change)
+        {
+
+            return amount_p;
+        }
+    }
+}
